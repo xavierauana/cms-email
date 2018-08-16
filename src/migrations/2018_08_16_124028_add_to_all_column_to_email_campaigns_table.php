b1f6c443ea22a052b3909115eede3671a3139546
Xavier Au <xavier.au@anacreation.com>
2018-08-16 17:14:56 +0800

    add migration

---
+++ src/migrations/2018_08_16_124028_add_to_all_column_to_email_campaigns_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Anacreation\CmsEmail\Models\Recipient;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddToAllColumnToEmailCampaignsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up() {
+        Schema::table('email_campaigns', function (Blueprint $table) {
+            $table->text("to_status")
+                  ->defalut(serialize([Recipient::StatusTypes['confirmed']]));
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down() {
+        Schema::table('email_campaigns', function (Blueprint $table) {
+            $table->dropColumn("to_status");
+        });
+    }
+}
